baf4f176fc00f66910a9f44f5ff993b8a9f4af51
Stuart Harris <stuart.harris@red-badger.com>
2018-08-20 12:15:36 +0100

    upgrade to gatsby2.0

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,41 +1,45 @@
 import React from "react";
 import Helmet from "react-helmet";
-import Link from "gatsby-link";
+import { graphql } from "gatsby";
+
+import Layout from "../components/layout";
 
 class BlogPostTemplate extends React.Component {
-    render() {
-        const post = this.props.data.markdownRemark;
-        const siteTitle = this.props.data.site.siteMetadata.title;
+  render() {
+    const post = this.props.data.markdownRemark;
+    const siteTitle = this.props.data.site.siteMetadata.title;
 
-        return (
-            <div>
-                <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
-                <h1>{post.frontmatter.title}</h1>
-                <p>{post.frontmatter.date}</p>
-                <div dangerouslySetInnerHTML={{ __html: post.html }} />
-                <hr />
-            </div>
-        );
-    }
+    return (
+      <Layout>
+        <div>
+          <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
+          <h1>{post.frontmatter.title}</h1>
+          <p>{post.frontmatter.date}</p>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          <hr />
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default BlogPostTemplate;
 
 export const pageQuery = graphql`
-    query BlogPostByPath($path: String!) {
-        site {
-            siteMetadata {
-                title
-                author
-            }
-        }
-        markdownRemark(frontmatter: { path: { eq: $path } }) {
-            id
-            html
-            frontmatter {
-                title
-                date(formatString: "MMMM DD, YYYY")
-            }
-        }
+  query BlogPostByPath($path: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
     }
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      id
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
 `;
